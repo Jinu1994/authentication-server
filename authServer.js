841a74cc7af05f57c45fc0925f0a4050b3b0a613
Jinu George <jinu.g@deltax.com>
2018-03-10 14:12:26 +0530

    Add GET request for individual users by ID

--- authServer.js
+++ authServer.js
@@ -4,6 +4,7 @@ const {User}=require('./models/user.js');
 const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
+const {ObjectId}=require('mongodb')
 
 app.use(bodyParser.json());
 
@@ -20,6 +21,22 @@ app.post('/user',(request,response)=>{
 
 app.get('/users',(request,response)=>{
    User.find({}).then((users)=>response.status(200).send({users}),(error)=>response.status(400).send(error));
+});
+
+app.get('/users/:id',(request,response)=>{
+    var userId=request.params.id;
+    if(!ObjectId.isValid(userId))
+        response.status(400).send(`UserId: ${userId} is invalid`);
+    else {
+        User.findById(userId)
+            .then((user)=>{
+            if(!user)
+                response.status(400)
+                        .send(`User with id ${userId} not found`);
+            else
+                response.status(200).send(user)
+            });
+    }
 });
 
 app.listen(3000,()=>{
